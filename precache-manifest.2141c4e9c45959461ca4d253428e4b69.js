6c7f2892329511e5a83445e37aedafc944c3ef2b
ajai lee <ajaidonglee@gmail.com>
2020-07-29 00:01:44 +0530

    Updates

---
+++ precache-manifest.2141c4e9c45959461ca4d253428e4b69.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f42e8302c11151c4909202a67ac02467",
+    "url": "/facedetectionheroku/index.html"
+  },
+  {
+    "revision": "ee5ac69ac75c7111fe3d",
+    "url": "/facedetectionheroku/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9b2bdd73446704fe5a40",
+    "url": "/facedetectionheroku/static/css/main.41c12541.chunk.css"
+  },
+  {
+    "revision": "ee5ac69ac75c7111fe3d",
+    "url": "/facedetectionheroku/static/js/2.872c4e59.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facedetectionheroku/static/js/2.872c4e59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b2bdd73446704fe5a40",
+    "url": "/facedetectionheroku/static/js/main.0ef0f74b.chunk.js"
+  },
+  {
+    "revision": "b5779ab3b775e277a3a9",
+    "url": "/facedetectionheroku/static/js/runtime-main.d68b6de1.js"
+  },
+  {
+    "revision": "9a5c7c7f69a116bf27c7b51f82d4028e",
+    "url": "/facedetectionheroku/static/media/brain.9a5c7c7f.jpg"
+  }
+]);
